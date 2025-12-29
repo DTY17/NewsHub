297a56e37997260c0c9a6ea30ff146be1dcfc6a2
DTY17 <dinanthemika.personal@gmail.com>
2025-12-30 02:04:16 +0530

    Fix CORS issue and update Home.tsx - 3

--- index.ts
+++ index.ts
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use(cors({
   origin: [ "http://localhost:5173", "https://news-hub-lime-two.vercel.app" ],
   methods: ["GET", "POST","OPTION","PUT"],        
-
 }));
 
 app.use('/api/abc/user',authrouter)
